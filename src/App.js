e548c48ee315c6267945ddf161940870927d3f0b
nolastemgarden <nolastemgarden@gmail.com>
2022-01-12 12:14:41 -0600

    Replace CRA template with stubs of my my components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,62 @@
+import { React, useState } from 'react'
+import { catData } from "./catData";
+
+// MUI components
+import { Container, Box, Typography } from '@mui/material'
 
 export default function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    catData.forEach(cat => {
+        console.log(`DATA for cat number ${cat.id}: ${JSON.stringify(cat, null, 4)}`);
+    });
+    
+  
+    return (
+        <ThemeProvider theme={theme}>
+            <Box id="root"
+                sx={{
+                    width: "100vw",
+                    height: "100vh",
+                    backgroundColor: "#efefef", // grey backdrop
+                    overflow: "hidden",
+                    display: 'flex',
+                    flexDirection: "column"
+                }}
+            >
+                <AppBody />
+
+                
+                <EditModal />
+            </Box>
+        </ThemeProvider>
+    )
+}
+
+function AppBody(props) {
+    return (
+        <Container maxWidth="lg" >
+            <Summary />
+            <Detail />
+        </Container>
+    )
+}
+
+function Summary(props) {
+    return (
+        <Box >
+        </Box>
+    )
+}
+
+function Detail(props) {
+    return (
+        <Box >
+        </Box>
+    )
+}
+
+function EditModal(props) {
+    return (
+        <Box >
+        </Box>
+    )
 }
