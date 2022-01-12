7ca26b14383a6b09322fb9416699f1d5f45eef70
nolastemgarden <nolastemgarden@gmail.com>
2022-01-12 17:34:14 -0600

    Add scrolling list of cat summaries

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import { catData } from "./catData";
 
 import { AppBar } from "./components/AppBar"
 // MUI components
-import { Container, Box, Typography } from '@mui/material'
+import { Container, Box, Avatar, Grid, Typography } from '@mui/material'
 import { Dialog } from '@mui/material'
+import { List, ListItem } from '@mui/material'
+
 // THEME
 import theme from "./theme"
 import { ThemeProvider } from '@mui/material'
@@ -13,8 +15,7 @@ let primary = theme.palette.primary.main
 
 
 export default function App() {
-    const [selectedCatId, setSelectedCatId] = useState(null)
-
+    const [selectedCatIds, setSelectedCatIds] = useState([])
     const [editModalOpen, setEditModalOpen] = useState(false)
 
     const toggleEditModal = (editModalOpen) => {
@@ -36,31 +37,106 @@ export default function App() {
                     backgroundColor: "#efefef", // grey backdrop
                     overflow: "hidden",
                     display: 'flex',
-                    flexDirection: "column"
+                    flexDirection: "column",
                 }}
             >
                 <AppBar />
-                <AppBody />
 
+                <Container
+                    maxWidth="lg"
+                    sx={{
+                        height: "100%",
+                        display: "flex"
+                    }} >
+                    <Summary />
+                    <Detail />
+                </Container>
                 
-                <EditModal />
+                <EditModal 
+                    open={editModalOpen}
+                />
             </Box>
         </ThemeProvider>
     )
 }
 
-function AppBody(props) {
-    return (
-        <Container maxWidth="lg" >
-            <Summary />
-            <Detail />
-        </Container>
-    )
-}
-
 function Summary(props) {
     return (
-        <Box >
+        <Box sx={{
+            flex: "0 1 35%",
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+        }}>
+            <SearchBox  />
+            <ListOfCats />
+            
+        </Box>
+    )
+}
+function SearchBox() {
+    return (
+        <Box sx={{
+            height: "25%",
+            border: `solid ${primary} 1px`,
+
+            
+        }}>
+
+
+        </Box>
+    )
+}
+function ListOfCats(props) {
+    return (
+        <List sx={{
+            height: "75%",
+            border: `solid ${primary} 1px`,
+            padding: "0 0 2rem",
+            overflowY: "scroll"
+        }}>
+            {
+                catData.map(cat => {
+                    return (
+                        <CatSummary key={cat.id} cat={cat} />
+                    )
+                })
+            }
+
+        </List>
+    )
+}
+function CatSummary(props) {
+    let { name, birthdate, owner_name, thumbnail_url } = props.cat
+    
+    let dateString = convertDateFormat(birthdate)
+    
+    
+    return (
+        <Box 
+            sx={{
+            // height: "25vh",
+            border: `solid ${primary} 1px`,
+            display: "flex",
+            flexDirection: "column",
+            padding: "1rem 2rem"
+
+        }}>
+            <Box sx={{ display: "flex", }}>
+                <Avatar
+                    variant="square"
+                    alt={name}
+                    src={thumbnail_url}
+                    sx={{
+                        border: 'solid #bbb 1px',
+                        width: '6rem',
+                        height: '6rem',
+                    }}
+                />
+                <Typography variant="h3" children={name} sx={{ padding: "1rem" }} />
+
+            </Box>
+            <Typography variant="h5" children={dateString} sx={{ padding: "1rem 1rem 0rem" }} />
         </Box>
     )
 }
@@ -116,4 +192,17 @@ function EditModal(props) {
             </Dialog>
         // </Box>
     )
+}
+
+function convertDateFormat(inputDate) {
+    let firstHyphen = inputDate.indexOf("-")
+    let secondHyphen = inputDate.lastIndexOf("-")
+    let year = inputDate.slice(0, firstHyphen)
+    let monthList = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"]
+    let monthNumber = Number(inputDate.slice(firstHyphen + 1, secondHyphen))
+    let month = monthList[(monthNumber - 1)]
+    let day = Number(inputDate.slice(secondHyphen + 1))
+    let date = `${day} ${month} ${year}`
+    console.log(`DATE: ${date}`);
+    return date
 }
