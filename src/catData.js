d1a16070428ae1186c3fdb9dae4f4ac949f2c849
nolastemgarden <nolastemgarden@gmail.com>
2022-01-12 12:16:34 -0600

    Create catData.js
    Copied from instructions

---
+++ src/catData.js
@@ -0,0 +1,42 @@
+export const catData = [
+    {
+        "id": 1,
+        "name": "Sam",
+        "birthdate": "2018-10-02",
+        "owner_name": "John Doe",
+        "thumbnail_url": "https://images.unsplash.com/photo-1596854273338-cbf078ec7071?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
+        "views_count": 0
+    },
+    {
+        "id": 2,
+        "name": "Kitty",
+        "birthdate": "2016-01-01",
+        "owner_name": "Jane Smith",
+        "thumbnail_url": "https://images.unsplash.com/photo-1615497001839-b0a0eac3274c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=668&q=80",
+        "views_count": 0
+    },
+    {
+        "id": 3,
+        "name": "Felix",
+        "birthdate": "2011-01-10",
+        "owner_name": "Sam Jones",
+        "thumbnail_url": "https://images.unsplash.com/photo-1607242792481-37f27e1d74e1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=668&q=80",
+        "views_count": 0
+    },
+    {
+        "id": 4,
+        "name": "Garfield",
+        "birthdate": "2020-02-10",
+        "owner_name": "Claire Morrison",
+        "thumbnail_url": "https://images.unsplash.com/photo-1604675223954-b1aabd668078?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=668&q=80",
+        "views_count": 0
+    },
+    {
+        "id": 5,
+        "name": "Poppy",
+        "birthdate": "2014-11-11",
+        "owner_name": "Jane Doe",
+        "thumbnail_url": "https://images.unsplash.com/photo-1580784355694-0d5295dcc007?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=668&q=80",
+        "views_count": 0
+    }
+]
