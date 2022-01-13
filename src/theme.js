9661f5435c74f2e852e7c974434561628fd3f5dc
nolastemgarden <nolastemgarden@gmail.com>
2022-01-13 17:51:50 -0600

    Add styles that affect form labels and textFields

--- src/theme.js
+++ src/theme.js
@@ -5,7 +5,13 @@ import {
 
 const theme = createTheme({
     components: {
-        
+        MuiTextField: {
+            styleOverrides: {  
+                root: {         
+                    height: "80px"
+                },
+            },
+        },
     },
     palette: {
         primary: {
@@ -22,6 +28,10 @@ const theme = createTheme({
         h3: {
             padding: "1rem"
         },
+        h4: {
+            padding: "0.2rem 1rem",
+            fontSize: "2rem"
+        },
         h5: {
             padding: "0.2rem 1rem",
             fontSize: "1.5rem"
@@ -30,7 +40,11 @@ const theme = createTheme({
             textTransform: "none",
             fontSize: "1.5rem"
 
-        }
+        },
+        body1: {
+            lineHeight: "80px",
+            fontSize: "1.5rem"
+        },
 
     },
     transitions: {
