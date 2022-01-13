6afc6de12711099d749762e5a1c23d89e7e6e4ef
nolastemgarden <nolastemgarden@gmail.com>
2022-01-13 13:09:31 -0600

    Add Edit and Delete buttons, relevant theming, and page layout

--- src/components/DetailPanel.js
+++ src/components/DetailPanel.js
@@ -1,2 +1,86 @@
+import { React, useMemo } from 'react'
+
 import { convertDateFormat } from "../lowLevelHelpers";
+// MUI components
+import { Box, Typography, Button } from '@mui/material'
+
+export function DetailPanel(props) {
+    let { validSelection, selectedCatId, selectedCatsData, toggleEditModal, deleteCat } = props
+    let { name, thumbnail_url, birthdate, owner_name, views_count } = selectedCatsData
+
+    // const dateString = useMemo(() => convertDateFormat(birthdate), [birthdate])  
+    const dateString = convertDateFormat(birthdate) 
+
+    let details = (validSelection) ?
+        <Box sx={{
+            display: "flex",
+            flexDirection: "column",
+            width: "70%",
+            flex: "1 0 80%"
+        }}>
+            <Box id="cat detail picture"
+                sx={{
+                    paddingTop: "75%",
+                    height: '0',
+                    width: '100%',
+                    backgroundImage: `url(${thumbnail_url})`,
+                    backgroundPosition: 'center',
+                    backgroundSize: 'cover',
+                }}
+            />
+            <Typography variant="h3" children={name} />
+            <Typography variant="h5" children={`Birthdate: ${dateString}`} />
+            <Typography variant="h5" children={`Owner: ${owner_name}`} />
+            <Typography variant="h5" children={`Number of views: ${views_count} times`} />
+        </Box>
+        :
+        <Typography variant="h3" children="Please Select a Cat" />
+
+
+    return (
+        <Box sx={{
+            flex: "0 0 75%",
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "flex-start",
+            alignItems: "center",
+            padding: "4rem 0rem"
+        }}>
+            {details}
+            <Buttons
+                toggleEditModal={toggleEditModal}
+                deleteCat={deleteCat}
+                validSelection={validSelection}
+            />
+        </Box>
+    )
+}
+function Buttons(props) {
+    let { toggleEditModal, deleteCat, validSelection } = props
+    
+    let display = validSelection ? "flex" : "none"
+    return (
+        <Box 
+            sx={{
+                display: display,
+                width: '100%',
+                flex: "0 0 25%",
+                justifyContent: "flex-end",
+                alignItems: "flex-start",
+                paddingRight: "15%"
+            }}
+        >
+            <Button 
+                children="Edit"
+                onClick={() => toggleEditModal()}
+            />
+            <Box sx={{ width: "0", height: "1.6rem", marginTop: "0.8rem", border: "solid black 1px" }} />
+            <Button
+                children="Delete"
+            />
+        </Box>
+    )
+}
+
 
--- src/theme.js
+++ src/theme.js
@@ -5,38 +5,7 @@ import {
 
 const theme = createTheme({
     components: {
-        // MuiPaper: {
-        //     styleOverrides: {  // Name of the slot
-        //         root: {        // Some CSS
-        //             backgroundColor: '#fff',
-        //         },
-        //     },
-        // },
-        // MuiIconButton: {
-        //     styleOverrides: {
-        //         root: {
-        //             color: "#999",
-        //             backgroundColor: "rgba(0, 0, 0, 0.0)",
-        //             "&:hover": {
-        //                 color: "#111",
-        //                 backgroundColor: "rgba(0, 0, 0, 0.0)",
-        //             },
-        //         },
-        //     },
-        // },
-        // MuiInput: {
-        //     styleOverrides: {
-        //         root: {
-        //             borderBottom: "solid #ccc 1.5px",
-        //             "&:focus": {
-        //                 borderBottom: "solid red 1px"
-        //             },
-        //         },
-        //         focussed: {
-        //             borderBottom: "solid #000 1.5px"
-        //         },
-        //     },
-        // }        
+        
     },
     palette: {
         primary: {
@@ -54,7 +23,13 @@ const theme = createTheme({
             padding: "1rem"
         },
         h5: {
-            padding: "0.2rem 1rem"
+            padding: "0.2rem 1rem",
+            fontSize: "1.5rem"
+        },
+        button: {
+            textTransform: "none",
+            fontSize: "1.5rem"
+
         }
 
     },
