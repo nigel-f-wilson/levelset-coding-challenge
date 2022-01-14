3af1b464b7519861bed46194b50334653102ae23
nolastemgarden <nolastemgarden@gmail.com>
2022-01-13 20:00:11 -0600

    Extract all components to base level of this file
    some were composed in order to avoid props drilling but I think I need them this way to get the multi field form working

--- src/components/EditModal.js
+++ src/components/EditModal.js
@@ -6,10 +6,13 @@ import DatePicker from '@mui/lab/DatePicker';
 import CloseIcon from '@mui/icons-material/Close';
 
 export function EditModal(props) {
-    let { open, selectedCatData, toggleEditModal } = props
+    let { open, selectedCatData, toggleEditModal, saveUpdates } = props
     // let { id, name, birthdate, owner_name, thumbnail_url } = selectedCatData
 
     let [catData, setCatData] = useState({selectedCatData})
+
+    let [editedCat, setEditedCat] = useState({ selectedCatData })
+
 
     function updateLocalCatData() {
         
@@ -48,108 +51,121 @@ export function EditModal(props) {
 
                 margin: "0"
             }}>
-            <Header />
-            <Form />
-            <SaveAndCancelButtons />
+            <Header 
+                toggleEditModal={toggleEditModal}
+            />
+            <Form 
+                catData={catData}
+            />
+            <SaveAndCancelButtons 
+                saveUpdates={saveUpdates}
+                editedCat={editedCat}
+                toggleEditModal={toggleEditModal}
+            />
 
         </Dialog>
     )
+}
 
-    function Header() {
-        return (
-            <Box sx={{
-                borderBottom: "solid #888 1px",
-                height: "75px",
+function Header(props) {
+    let { toggleEditModal } = props
+    return (
+        <Box sx={{
+            borderBottom: "solid #888 1px",
+            height: "75px",
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            padding: "0 1rem"
+
+        }} >
+            <Typography variant="h4" children="Edit Cat" />
+            <IconButton
+                children={<CloseIcon />}
+                onClick={toggleEditModal}
+            />
+        </Box>
+    )
+}
+function Form(props) {
+    let { catData } = props
+
+    return (
+        <Box sx={{
+            height: "550px",
+            display: "flex",
+            padding: "0 1rem"
+        }} >
+            <FormLabels />
+            <FormInputs
+                catData={catData}
+            />
+        </Box>
+    )
+
+
+}
+function FormLabels() {
+    return (
+        <Box id="form_labels"
+            sx={{
+                flex: "0 0 35%",
+                paddingLeft: "2rem",
                 display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-                padding: "0 1rem"
-
+                flexDirection: "column",
+                justifyContent: "center"
             }} >
-                <Typography variant="h4" children="Edit Cat" />
-                <IconButton
-                    children={<CloseIcon />}
-                    onClick={toggleEditModal}
-                />
-            </Box>
-        )
-    }
-    function Form() {
-        return (
-            <Box sx={{
-                height: "550px",
+            <Typography
+                variant="body1"
+                children={"Thumbnail URL"}
+            />
+            <Typography
+                variant="body1"
+                children={"Name"}
+            />
+            <Typography
+                variant="body1"
+                children={"Birth date"}
+            />
+            <Typography
+                variant="body1"
+                children={"Owner"}
+            />
+        </Box>
+    )
+}
+function FormInputs(props) {
+    let { catData } = props
+    let { id, name, birthdate, owner_name, thumbnail_url } = catData
+
+
+    console.log(`FORM INPUTS HAS DATA: ${JSON.stringify(catData, null, 4)}`);
+
+    console.log(`name VALUE should be set to: ${name}`);
+
+
+    return (
+        <Box id="form_Inputs"
+            sx={{
+                flex: "1 1 65%",
                 display: "flex",
-                padding: "0 1rem"
+                flexDirection: "column",
+                justifyContent: "center"
             }} >
-                <FormLabels />
-                <FormInputs 
-                    catData={catData}
-                />
-            </Box>
-        )
+            <TextField
+                value={thumbnail_url}
+                variant="outlined"
 
-        
-    }
-    function FormLabels() {
-        return (
-            <Box id="form_labels"
-                sx={{
-                    flex: "0 0 35%",
-                    paddingLeft: "2rem",
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center"
-                }} >
-                <Typography
-                    variant="body1"
-                    children={"Thumbnail URL"}
-                />
-                <Typography
-                    variant="body1"
-                    children={"Name"}
-                />
-                <Typography
-                    variant="body1"
-                    children={"Birth date"}
-                />
-                <Typography
-                    variant="body1"
-                    children={"Owner"}
-                />
-            </Box>
-        )
-    }
-    function FormInputs(props) {
-        let { id, name, birthdate, owner_name, thumbnail_url } = props.catData
-
-        
-        console.log(`FORM INPUTS HAS DATA: ${JSON.stringify(catData, null, 4)}`);
-
-        console.log(`name VALUE should be set to: ${name}`);
-        
-        
-        return (
-            <Box id="form_Inputs"
-                sx={{
-                    flex: "1 1 65%",
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center"
-                }} >
-                <TextField
-                    value={thumbnail_url}
-                    variant="outlined"
-
-                />
-                <TextField
-                    value={name}
-                    onChange={updateName}
-                    variant="outlined"
+            />
+            <TextField
+                value={name}
+                // onChange={updateName}
+                variant="outlined"
 
 
 
-                />
-                {/* <DatePicker
+            />
+            {/* <DatePicker
                         value={catData.birthdate}
                         variant="outlined"
                         onChange={}
@@ -157,51 +173,52 @@ export function EditModal(props) {
 
 
                     /> */}
-                <TextField
-                    value="Test"
-                    variant="outlined"
+            <TextField
+                value="Test"
+                variant="outlined"
 
 
 
-                />
-                <TextField
-                    value="Test"
-                    variant="outlined"
+            />
+            <TextField
+                value="Test"
+                variant="outlined"
 
 
 
-                />
-            </Box>
-        )
-    }
+            />
+        </Box>
+    )
+}
 
-    function SaveAndCancelButtons(props) {
-        return (
-            <Box
-                sx={{
-                    display: "flex",
-                    width: '100%',
-                    height: "75px",
-                    justifyContent: "flex-end",
-                    alignItems: "center",
-                    borderTop: "solid #888 1px",
+function SaveAndCancelButtons(props) {
+    let { saveUpdates, editedCat, toggleEditModal } = props
 
-                }}
-            >
-                <Button
-                    children="Save"
-                // onClick={() => toggleEditModal()}
-                />
-                <Box sx={{ width: "0", height: "1.6rem", margin: "0.8rem 0.5rem", border: "solid black 1px" }} />
-                <Button
-                    children="Cancel"
-                    onClick={toggleEditModal}
-                    sx={{  paddingRight: "2rem", }}
-                />
-            </Box>
-        )
-    }
+    return (
+        <Box
+            sx={{
+                display: "flex",
+                width: '100%',
+                height: "75px",
+                justifyContent: "flex-end",
+                alignItems: "center",
+                borderTop: "solid #888 1px",
 
+            }}
+        >
+            <Button
+                children="Save"
+                onClick={() => { saveUpdates(editedCat) }}
+            // onClick={saveUpdates(editedCat)}
+            />
+            <Box sx={{ width: "0", height: "1.6rem", margin: "0.8rem 0.5rem", border: "solid black 1px" }} />
+            <Button
+                children="Cancel"
+                onClick={toggleEditModal}
+                sx={{ paddingRight: "2rem", }}
+            />
+        </Box>
+    )
 }
 
 
