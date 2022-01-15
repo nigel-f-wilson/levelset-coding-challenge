45cf300173aa7a0cee1af3c8c331f07b05f2ca44
nolastemgarden <nolastemgarden@gmail.com>
2022-01-15 07:58:32 -0600

    Switch to using HTML native <input type="date">

--- src/components/inputs/BirthdateInput.js
+++ src/components/inputs/BirthdateInput.js
@@ -1,33 +1,30 @@
-import { React, useState } from 'react'
+import { React } from 'react'
 import { FormRow } from "./FormRow";
 
-import { Box, Typography, TextField } from '@mui/material'
-import DesktopDatePicker from '@mui/lab/DesktopDatePicker';
-
+import { TextField } from '@mui/material'
+import { convertDateFormat } from '../../lowLevelHelpers';
 
 export function BirthdateInput(props) {
-    let { formBirthdate, handleFormChange, handleDateChange } = props
+    let { formBirthdate, handleFormChange } = props
 
-    const [date, setDate] = useState(formBirthdate)
+    let dateString = convertDateFormat(formBirthdate)
+    console.log(`Form BD: ${formBirthdate}`);
 
     return (
         <FormRow label={"Birthdate"} >
-            <DesktopDatePicker
-                name="birthdate"
-                value={formBirthdate}
-                onChange={(newDate) => {
-                    setDate(newDate);
-                }}
-                onAccept={() => handleDateChange(date)}
-                renderInput={(params) => <TextField {...params} />}
-            />
-            {/* <TextField
+            <TextField
                 name="birthdate"
                 value={formBirthdate}
                 variant="outlined"
                 onChange={handleFormChange}
-                fullWidth 
-            /> */}
+                id="date"
+                type="date"
+                fullWidth
+                // Still trying to get this to display this string as "dd mon yyyy" instead of "mm/dd/yyyy"
+                // InputProps={{
+                //     valueAsDate: dateString,
+                // }}  
+            />
         </FormRow>
     )
 }
